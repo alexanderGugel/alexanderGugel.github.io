14e79c15d09e2b966fa74d8ee5acdac4b81f6c51
Alexander Gugel <alexander.gugel@gmail.com>
2015-08-07 15:56:49 +0200

    Don't display game on mobile devices

--- js/index.js
+++ js/index.js
@@ -3,14 +3,19 @@ import fit from 'canvas-fit'
 import loop from 'raf-loop'
 import throttle from 'lodash.throttle'
 
-let canvas = document.getElementById('game')
-let context = canvas.getContext('2d')
+var isTouchDevice = 'ontouchstart' in document.documentElement
 
-let game = new Game(context)
+if (!isTouchDevice) {
+  let canvas = document.createElement('canvas')
+  let context = canvas.getContext('2d')
+  let game = new Game(context)
 
-loop(throttle(() => game.step(), (1 / 0.06) * 2)).start()
+  document.body.appendChild(canvas)
 
-window.addEventListener('resize', fit(canvas), false)
-window.addEventListener('resize', () => game.reset(), false)
+  loop(throttle(() => game.step(), (1 / 0.06) * 2)).start()
 
-document.addEventListener('keydown', (e) => game.onKeydown(e))
+  window.addEventListener('resize', fit(canvas), false)
+  window.addEventListener('resize', () => game.reset(), false)
+
+  document.addEventListener('keydown', (e) => game.onKeydown(e))
+}
